33dca5647658ad5a9b31524f913ff07de8cb724f
Vaibhav Potdar <vaibhav.potdar9946@gmail.com>
2021-07-24 13:38:07 +0530

    Temp commit

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,11 +5,20 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
+
+
 export class HeaderComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+    window.onscroll = function() {scrollFunction()};
+    
   }
-
 }
+function scrollFunction() {
+  if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("header-back").style.background = "rgba(26,32,44,1)";
+  }
+}
+
